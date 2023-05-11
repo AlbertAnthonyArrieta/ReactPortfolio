3b86d9d1f1a7f9e1d8a6f8a9628f79b9df218afc
Albert Arrieta <albert.arrieta@uleth.ca>
2023-05-10 23:25:19 -0600

    scroll queue and animation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import HeroSection from "./components/HeroSection";
 import Navigation from './components/Navigation';
 import PageContent from './components/PageContent';
-import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,29 +1,40 @@
 import Container from 'react-bootstrap/Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons';
+
 export const HeroSection = () => {
 
 	return (
 		<div id="section--hero">
-			{/* <canvas id="particleArt"></canvas>
-            <script src="./scripts/particles.js"></script> */}
-			<Container id='section--hero__text'>
-					<h1 className='text--big'>Hello!</h1>
-					<h1>
-						My name is
-						<span className='color--white'> Albert Arrieta</span>.<br></br>
-						I am a
-						<span className='color--cyan'> Software Developer </span>
-						who is driven to create
-						<span className='color--pink'> Functional</span>
-						,
-						<span className='color--pink'> Elegant</span>
-						,
-						and
-						<span className='color--pink'> User-friendly </span>
-						code.
+			<div id="section--hero__main">
+				<div id="section--hero__main">
+					<Container id='section--hero__text'>
+						<h1 className='text--big'>Hello!</h1>
+						<h1>
+							My name is
+							<span className='color--white'> Albert Arrieta</span>.<br></br>
+							I am a
+							<span className='color--cyan'> Software Developer </span>
+							who is driven to create
+							<span className='color--pink'> Functional</span>
+							,
+							<span className='color--pink'> Elegant</span>
+							,
+							and
+							<span className='color--pink'> User-friendly </span>
+							code.
+						</h1>
+					</Container>
+				</div>
 
-					</h1>
-			</Container>
+			</div>
+			<div id='container--scroller'>
+				<p className='color--white'>Take a Look!</p>
+				<FontAwesomeIcon className='color--cyan' icon={faAngleDoubleDown} size="2xl" />
+			</div>
 		</div>
+
+
 	)
 }
 export default HeroSection
