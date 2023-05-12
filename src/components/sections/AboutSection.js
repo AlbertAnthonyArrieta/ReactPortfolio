edb22fa9a21ede27d03e6a6069908e4f0da87da9
Albert Arrieta <albert.arrieta@uleth.ca>
2023-05-11 21:10:02 -0600

    not gonna go with honeycomb design anymore since too complex

--- src/components/sections/AboutSection.js
+++ src/components/sections/AboutSection.js
@@ -4,18 +4,30 @@ import ProfilePic from '../../images/profile.png';
 export const AboutSection = () => {
 
     return (
-        <div className='about'>
+        <div className='section about'>
             <Container>
                 <h1 className='section--title'>ABOUT ME</h1>
-                <ImageAndDesc
-                    img={ProfilePic}
-                    heading={"Who is this guy?"}
-                    // paragraph={'I am a young aspiring software developer located in Calgary, Alberta with about 4 years of programming experience. I am a proud SAIT Alumni graduating from the IT Software Development Diploma program and I am now pursuing a degree in Computer Science at the University of Lethbridge to broaden my skills as a software developer.'}
-                    paragraph={
-                        "Hi there! I'm Albert, a Computer Science major in my last year at The University of Lethbridge, with a minor in New Media. I specialize in front-end development, utilizing my creative ideas and eye for design to construct remarkable web applications. Furthermore, I hold a keen interest in full-stack development and aspire to develop robust, top-notch software."}
-                />
-                <div className='about--technologies'>
-                    <h2 className='section--subheading section--subheading__technologies'>Technologies</h2>
+                <div className='about--subcontainer'>
+                    <ImageAndDesc
+                        img={ProfilePic}
+                        heading={"Who is this guy?"}
+                        paragraph={
+                            "Hi there! I'm Albert, a Computer Science major in my last year at The University of Lethbridge, with a minor in New Media. I specialize in Front-end development, utilizing my creative ideas and eye for design to construct remarkable web applications. Furthermore, I hold a keen interest in full-stack development and aspire to develop robust, top-notch software."}
+                    />
+                </div>
+                <div className='about--subcontainer'>
+                    <h2 className='section--subheading'>Technologies</h2>
+                    <div className='about--container'>
+                        <div className='about--item'>
+                            <h3>Programming Languages</h3>
+                        </div>
+                        <div className='about--item'>
+                            <h3>Frameworks</h3>
+                        </div>
+                        <div className='about--item'>
+                            <h3>Tools</h3>
+                        </div>
+                    </div>
                 </div>
             </Container>
         </div>
